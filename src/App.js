390a4e11f39d48fe7d184c84a8cfcdb15f918df7
RayW0 <raywordemann@gmail.com>
2023-10-02 20:19:50 +0600

    changed widget to popup style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios'
+import {ReactComponent as PhoneLogo} from './svg/phone_1-svg.svg'
 
 
 function App() {
 
+  const [chat_logs, setChatLogs] = useState([])
+
   useEffect(() => {
+    handleLog()
     let chat_button = document.getElementById('popup');
     chat_button.addEventListener('click', openForm);
   })
@@ -18,23 +22,40 @@ function App() {
     }
     console.log(document.getElementById("block"), "triggered openForm")
   }
-  const changeBackground = () => {
-    if (document.body.className === "wee"){
-        setTimeout(() => {
-            document.body.className = "woo"
-        }, 3000)
-    }
-    else{
-        setTimeout(() => {
-            document.body.className = "wee"
-        }, 250)
-    }
-  }
+  
   const createChatLog = (message) => {
     let request = axios.get(`http://127.0.0.1:8000/create-chat-log?message=${message}`, {message: message})
     return request
   }
 
+  const getChatLog = () => {
+    let request = axios.get(`http://127.0.0.1:8000/get-chat-log`, {withCredentials: false})
+    return request
+  }
+
+  const handleLog = () => {
+    getChatLog()
+    .then((res) => {
+      console.log('REEEEES', res.data)
+      if (res.data.res === 'success'){
+        let my_data = res.data.chat_logs
+        let output = document.getElementById('output')
+        for (let i=0; i<my_data.length; i++){
+          output.value += my_data[i].date_of_creation + '       ' +  my_data[i].message + '\n'
+        }
+      }
+    })
+    .catch((err) => console.log(err))
+  }
+  const toggleChatView = () => {
+    let chatBlock = document.getElementById('block')
+    if (chatBlock.style.display === 'block'){
+      chatBlock.style.display = 'none'
+    }
+    else{
+      chatBlock.style.display = 'block'
+    }
+  }
   const sendMessage = () => {
     let input = document.getElementById("input")
     let output = document.getElementById("output")
@@ -46,7 +67,7 @@ function App() {
     .catch((err) => {
       console.log(err)
     })
-    output.value += "\n"+"Я: "+ contents
+    output.value += contents + '\n'
     input.value = ''
   }
   const catchEnterKey = (event) => {
@@ -63,17 +84,43 @@ function App() {
             <img style={my_styles} src="img/phone_1.png" alt="phone" />
         </button>
 
-        <div id="block">
+        <div id="block" style={{
+              width:"20%", 
+              right: "55px",
+              bottom: "55px",
+              position: 'absolute',
+              backgroundColor: '#f2f1ed',
+              padding: '2%',
+              height: '60vh',
+              display: 'none',
+              }} >
             <div class="d-flex flex-column justify-content-around" action="">
                 <h2>Чат</h2>
-            
                 <label for="msg"><b>Сообщение</b></label>
-                <textarea rows="15" placeholder="Введите сообщение.." name="msg" required id="output"></textarea>
+                <textarea readOnly rows="15" placeholder="Введите сообщение.." name="msg" required id="output"></textarea>
                 <input type="text" onKeyDown={catchEnterKey} placeholder="Введите сообщение.." name="msg" required id="input" ></input>
                 <button onClick={() => sendMessage()} type="button" class="btn bg-primary align-items-center d-flex" style={{width: "auto", color: "aliceblue"}}>Отправить</button>
             </div>
         </div>
+
+            <button
+            onClick={() => {toggleChatView()}}
+            class='widget'
+            style={{
+              width:"60px", 
+              height:"60px", 
+              borderRadius:"100%", 
+              position:"fixed",
+              border: 'unset',
+              right: "15px",
+              bottom: "15px", 
+              backgroundColor:"#31BD58",
+              }}
+              >
+            <PhoneLogo height={35} width={35} />
+            </button>
     </div>
+    
   );
 }
 
